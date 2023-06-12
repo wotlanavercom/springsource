12b653c838b83f5990ffe228df28cc61d049794d
shin <wotla3009@naver.com>
2023-06-12 17:49:43 +0900

    6.12 수업

---
+++ boot_naver/src/main/java/com/example/restaurant/BootNaverApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootNaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootNaverApplication.class, args);
+	}
+
+}
